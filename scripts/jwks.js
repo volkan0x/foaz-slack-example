42c44738f6a1461600326c30d9a4700953361d17
Volkan Bozkurt <20183360068@ogr.akdeniz.edu.tr>
2025-04-28 12:22:16 +0000

    fix: ensure the public directory exists

--- scripts/jwks.js
+++ scripts/jwks.js
@@ -4,6 +4,9 @@ import fs from "fs/promises"
 const generate = async () => {
     const keyStore = jose.JWK.createKeyStore();
 
+    // ensure the 'public' directory exists
+    await fs.mkdir("public", { recursive: true });
+
     await keyStore.generate("RSA", 2048, { alg: "RS256", use: "sig" });
     await fs.writeFile("public/keys.json", JSON.stringify(keyStore.toJSON(true), null, "  "))
     await fs.writeFile("public/jwks.json", JSON.stringify(keyStore.toJSON(), null, "  "))
